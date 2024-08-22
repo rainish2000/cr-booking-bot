def4639f793526b9ee3319e7a0e483db532e24a0
rainish2000 <rai.nishant.2000@gmail.com>
2024-08-22 16:25:47 +0800

    update script

--- script.sh
+++ script.sh
@@ -7,4 +7,6 @@ npm install pm2 -g -y
 yum install postgresql15.x86_64 postgresql15-server -y
 yum install postgresql-devel python3-devel
 git clone https://github.com/rainish2000/cr-booking-bot.git
+cd cr-booking-bot
 pip install -r requirements.txt
+pm2 start bot.py
