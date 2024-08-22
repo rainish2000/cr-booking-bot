92dd01c874a7904153adc8f76ae67ba4654d77dc
rainish2000 <rai.nishant.2000@gmail.com>
2024-08-22 18:31:40 +0800

    remove print

--- bot.py
+++ bot.py
@@ -331,7 +331,6 @@ async def confirm_delete_booking(update: Update, context: CallbackContext) -> in
     user = update.message.from_user
     username = user.username
     booking_id = update.message.text
-    print (booking_id.type)
 
     if booking_id == "cancel":
         await update.message.reply_text('Operation cancelled.')
